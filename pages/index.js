d846e777f8b1a5eb028d827fae29f62b329f0b44
erozonachi <erozonachi.james@gmail.com>
2019-08-13 22:29:54 +0100

    Add tv shows list to index file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
 /* const PostLink = props => (
   <li>
@@ -21,7 +22,7 @@ const Index = () => (
   </Layout>
 );
 
-export default Index; */
+export default Index;
 const PostLink = props => (
   <li>
     <Link href="/p/[id]" as={`/p/${props.id}`}>
@@ -41,4 +42,31 @@ export default function Blog() {
       </ul>
     </Layout>
   );
-}
+} */
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
