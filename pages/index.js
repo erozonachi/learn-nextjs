1acb2c81abea0f90961797f69e68306d7cb69b69
erozonachi <erozonachi.james@gmail.com>
2019-08-13 23:00:58 +0100

    Style nested components

--- pages/index.js
+++ pages/index.js
@@ -79,41 +79,46 @@ function getPosts() {
   ];
 }
 
+const PostLink = ({ post }) => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${post.id}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+);
+
 export default function Blog() {
   return (
     <Layout>
       <h1>My Blog</h1>
       <ul>
         {getPosts().map(post => (
-          <li key={post.id}>
-            <Link href="/p/[id]" as={`/p/${post.id}`}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
+          <PostLink key={post.id} post={post} />
         ))}
       </ul>
       <style jsx>{`
-        h1,
-        a {
+        h1 {
           font-family: 'Arial';
         }
 
         ul {
           padding: 0;
-        }
-
-        li {
-          list-style: none;
-          margin: 5px 0;
-        }
-
-        a {
-          text-decoration: none;
-          color: blue;
-        }
-
-        a:hover {
-          opacity: 0.6;
         }
       `}</style>
     </Layout>
