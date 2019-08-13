30e9f9a7ecaa53495583a5cfe6db9e0a978caa7a
erozonachi <erozonachi.james@gmail.com>
2019-08-13 22:03:28 +0100

    Use dynamic routing in index file

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
 
-const PostLink = props => (
+/* const PostLink = props => (
   <li>
     <Link href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
@@ -21,4 +21,24 @@ const Index = () => (
   </Layout>
 );
 
-export default Index;
+export default Index; */
+const PostLink = props => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
+    </Link>
+  </li>
+);
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink id="hello-nextjs" />
+        <PostLink id="learn-nextjs" />
+        <PostLink id="deploy-nextjs" />
+      </ul>
+    </Layout>
+  );
+}
